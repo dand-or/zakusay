254920d413e7cd5d29cbc3d083aea35c968c86c5
d-ando <moruze.orange@gmail.com>
2020-08-10 00:28:16 +0900

    randomize color

--- zakusay/Program.cs
+++ zakusay/Program.cs
@@ -15,9 +15,16 @@ namespace zakusay
 
         static void Main(string[] args)
         {
+            Console.ForegroundColor = GetConsoleColor();
             var words = args.Length > 0 ? args[0] : DateTime.Now.ToString();
             var output = zaku.Replace("replacer", words);
             Console.WriteLine(output);
         }
+
+        static private ConsoleColor GetConsoleColor()
+        {
+            var seed = DateTimeOffset.Now.ToUnixTimeSeconds() % Enum.GetNames(typeof(ConsoleColor)).Length;
+            return (ConsoleColor)Enum.ToObject(typeof(ConsoleColor), seed);
+        } 
     }
 }
