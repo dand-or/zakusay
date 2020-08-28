38c7462866ec73908d29fc4f944fef5fca5b0226
d-ando <moruze.orange@gmail.com>
2020-08-29 00:19:45 +0900

    add OperationContext

---
+++ zakusay/Domains/IOperationContext.cs
@@ -0,0 +1,10 @@
+﻿namespace zakusay.Domains
+{
+    public interface IOperationContext
+    {
+        bool GetIsCommander();
+        string GetMobileSuitDirName();
+        OperationContext.OperationMode GetOperationMode();
+        string GetWord();
+    }
+}
