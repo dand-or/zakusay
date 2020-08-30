5b5a2d92c882a84d1d52f0d39665a98ad372a6d1
d-ando <moruze.orange@gmail.com>
2020-08-30 22:55:11 +0900

    add normal controller

---
+++ zakusay/Controllers/NormalController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using zakusay.Domains;
+using zakusay.Repositories;
+
+namespace zakusay.Controllers
+{
+    public class NormalController : IController
+    {
+        private const string REPLACER = "replacer";
+        private readonly IOperationContext _context;
+        private readonly IMobileSuitArtRepository _repository;
+
+        public NormalController(IOperationContext context, IMobileSuitArtRepository repository)
+        {
+            this._context = context;
+            this._repository = repository;
+        }
+
+        public void View()
+        {
+            var template = this._repository.GetMobileSuitTemplate(this._context.GetMobileSuitDirName(), this._context.GetIsCommander());
+            Console.WriteLine(template.Replace(REPLACER, this._context.GetWord()));
+        }
+    }
+}
